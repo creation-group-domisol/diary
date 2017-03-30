feec56ed2d1ee94e353dca87bdb54bef636c9d6b
cob.b <bae@090713>
2017-03-30 10:47:59 +0900

    basic frame

--- app/src/main/java/com/group/creation/domisol/diary/MainActivity.java
+++ app/src/main/java/com/group/creation/domisol/diary/MainActivity.java
@@ -2,12 +2,25 @@ package com.group.creation.domisol.diary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import com.group.creation.domisol.diary.calendar.CalendarFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private CalendarFragment calendarFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calendarFragment = new CalendarFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.main_content, calendarFragment).commit();
+    }
+
+    public void onToCalendar() {
+        if (calendarFragment.isVisible())
+            return;
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, calendarFragment).commit();
     }
 }
---
+++ app/src/main/java/com/group/creation/domisol/diary/calendar/CalendarFragment.java
@@ -0,0 +1,23 @@
+package com.group.creation.domisol.diary.calendar;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.group.creation.domisol.diary.R;
+
+/**
+ * Created by cob on 2017. 3. 29..
+ */
+
+public class CalendarFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_calendar, container, false);
+        return rootView;
+    }
+}
